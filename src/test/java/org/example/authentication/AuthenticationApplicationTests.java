4d69dc9282e993cf9a1b976e392d7869381435a8
NgenoMark <kipchumbamark48@gmail.com>
2025-02-26 14:18:07 +0300

    Added testing of app

--- src/test/java/org/example/authentication/AuthenticationApplicationTests.java
+++ src/test/java/org/example/authentication/AuthenticationApplicationTests.java
@@ -2,7 +2,10 @@ package org.example.authentication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("dev")
 @SpringBootTest
 class AuthenticationApplicationTests {
 
